858d1351d37c8f822c8e769bfbaa506f3aca54ae
Melany Pedemonte <melany.pedemonte@coodesoft.com.ar>
2021-03-30 11:29:02 -0300

    dump de la db

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=localhost;dbname=db_app_citas',
+    'username' => 'user_app_citas',
+    'password' => 'pass_app_citas',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
