336f838830874907ccb3487864a6c5b68f49157c
Laion <leonardo.navarro@coodesoft.com.ar>
2021-04-07 18:08:54 -0300

    se agregan endpoint para los recursos

---
+++ controllers/ChatRoomController.php
@@ -0,0 +1,11 @@
+<?php
+namespace app\controllers;
+
+use yii\rest\ActiveController;
+
+
+class ChatRoomController extends BaseController {
+
+    public $modelClass = 'app\models\ChatRoom';
+
+}
---
+++ controllers/ProfileController.php
@@ -0,0 +1,11 @@
+<?php
+namespace app\controllers;
+
+use yii\rest\ActiveController;
+
+
+class ProfileController extends BaseController {
+
+    public $modelClass = 'app\models\Profile';
+
+}
