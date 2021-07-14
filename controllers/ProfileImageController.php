03cfbad6e62ae22d0220831a408b5b0515df29ae
BuildTools <lucho.2012.tandil@gmail.com>
2021-07-14 17:50:45 -0300

    Se vuelve a modificar el controller, se quitan referencias a los actions aun no imlementados

--- controllers/ProfileImageController.php
+++ controllers/ProfileImageController.php
@@ -11,8 +11,6 @@ class ProfileImageController extends BaseController {
     public function actions(){
         $actions = parent::actions();
         $actions['create']['class'] = 'app\actions\CreateImageAction';
-        $actions['update']['class'] = 'app\actions\UpdateImageAction';
-        $actions['delete']['class'] = 'app\actions\DeleteImageAction';
         return $actions;
     }
 }
