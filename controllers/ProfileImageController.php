5096cf30b4ec5b4cc83e48d17a2580433d324cc5
BuildTools <lucho.2012.tandil@gmail.com>
2021-07-14 15:52:51 -0300

    En ProfileImageController, se setean los action de creación, edición y eliminación de imágenes

--- controllers/ProfileImageController.php
+++ controllers/ProfileImageController.php
@@ -8,4 +8,11 @@ class ProfileImageController extends BaseController {
 
     public $modelClass = 'app\models\ProfileImage';
 
+    public function actions(){
+        $actions = parent::actions();
+        $actions['create']['class'] = 'app\actions\CreateImageAction';
+        $actions['update']['class'] = 'app\actions\UpdateImageAction';
+        $actions['delete']['class'] = 'app\actions\DeleteImageAction';
+        return $actions;
+    }
 }
