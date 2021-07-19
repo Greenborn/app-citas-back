163d97f6633efa1663e38575c2494af45c09b840
Lucho <lucho.2012.tandil@gmail.com>
2021-07-19 21:50:00 +0000

    Update db.php

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=appcitas',
-    'username' => 'appcitas',
-    'password' => '1234',
+    'dsn' => 'mysql:host=localhost;dbname=db_citas',
+    'username' => 'user_citas',
+    'password' => 'z0QBAuqozDPwDqdr',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
