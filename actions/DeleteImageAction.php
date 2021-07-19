0ff26f6de8b2ceff68dd150fb6fd23b2f0587c00
BuildTools <lucho.2012.tandil@gmail.com>
2021-07-19 16:42:15 -0300

    Se aplican correcciones

--- actions/DeleteImageAction.php
+++ actions/DeleteImageAction.php
@@ -9,25 +9,35 @@ use app\models\ProfileImage;
 class DeleteImageAction extends DeleteAction {
 
     public function run($id) {
-      $params = Yii::$app->getRequest()->getBodyParams();
-
       $response = Yii::$app->getResponse();
       $response->format = \yii\web\Response::FORMAT_JSON;
 
       $profile_image = ProfileImage::find()->where(['id' => $id])->one();
 
       if ($profile_image !== null){
-        if (!unlink($profile_image->path)) {
-            throw new \Exception('No se pudo eliminar el archivo', 1);
-        } else {
-            if ( $profile_image->delete() ){
-              $response->data = [
-                'success' => true,
-              ];
+        $transaction = ProfileImage::getDb()->beginTransaction();
+        if ( $profile_image->delete() ){
+
+          try {
+            if (!unlink($profile_image->path)) {
+                throw new \Exception('No se pudo eliminar el archivo', 1);
             } else {
-              throw new \Exception( $profile_image->getErrors(), 1);
+                $response->data = [
+                  'success' => true,
+                ];
             }
+          } catch (\Exception $e) {
+              $transaction->rollBack();
+              $response->data = [
+                'success' => false,
+                'message' => $e->getMessage()
+              ];
+          }
+
+        } else {
+          throw new \Exception( $profile_image->getErrors(), 1);
         }
+
       }
     }
 
