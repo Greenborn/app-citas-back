5e4dcb06e58f6921d0168ac24ed8d63a91d5241d
BuildTools <lucho.2012.tandil@gmail.com>
2021-07-21 16:54:38 -0300

    se agrega nuevo campo de url, se quita el disparo de excepciones para ser camibiado por la respuesta en json

--- actions/CreateImageAction.php
+++ actions/CreateImageAction.php
@@ -23,29 +23,45 @@ class CreateImageAction extends CreateAction {
             $temp   = $profile_img['tmp_name'];
             //Se comprueba si el archivo a cargar es correcto observando su extensión y tamaño
             if (!((strpos($tipo, "gif") || strpos($tipo, "jpeg") || strpos($tipo, "jpg") || strpos($tipo, "png")) && ($tamano < 2000000))) {
-              throw new \Exception('Error. La extensión o el tamaño de los archivos no es correcta.', 1);
+              $response->data = [
+                'success' => false,
+                'message' => 'Error. La extensión o el tamaño de los archivos no es correcta.'
+              ];
             } else {
               $date     = new \DateTime();
               $img_name = $date->getTimestamp().$profile_img['name'];
               //Si la imagen es correcta en tamaño y tipo
               //Se intenta subir al servidor
               $full_path = getcwd().'/user_data/'.$img_name;
-              if (move_uploaded_file($temp, $full_path)) {
-                $profImgModel             = new ProfileImage();
-                $profImgModel->path       = $full_path;
-                $profImgModel->profile_id = $params['profile_id'];
-                if ( $profImgModel->save() ){
-                  $response->data = [
-                    'id'   => $profImgModel->id,
-                    'path' => $profImgModel->path,
-                  ];
-                } else {
-                  throw new \Exception($profImgModel->getErrors(), 1);
+
+              try {
+                if (move_uploaded_file($temp, $full_path)) {
+                  $profImgModel             = new ProfileImage();
+                  $profImgModel->path       = $full_path;
+                  $profImgModel->url        = '/user_data/'.$img_name;
+                  $profImgModel->profile_id = $params['profile_id'];
+
+                  if ( $profImgModel->save() ){
+                    $response->data = [
+                      'success' => true,
+                      'id'      => $profImgModel->id,
+                      'url'     => $profImgModel->url
+                    ];
+                  } else {
+                    $response->data = [
+                      'success' => false,
+                      'message' => $profImgModel->getErrors()
+                    ];
+                  }
+
                 }
-              } else {
-                 //Si no se ha podido subir la imagen, mostramos un mensaje de error
-                 throw new \Exception('Ocurrió algún error al subir el fichero. No pudo guardarse.', 1);
+              } catch (\Exception $e) {
+                  $response->data = [
+                    'success' => false,
+                    'message' => $e
+                  ];
               }
+
           }
        }
 
